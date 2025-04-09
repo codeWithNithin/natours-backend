12ab74924b9a04f67be1dbe0a085d592f118597f
nithin kumar <nithinkottary25@gmail.com>
2025-04-09 15:54:34 +0530

    Feat: globlar err handler in progress

--- src/app.js
+++ src/app.js
@@ -11,4 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes)
 
+app.all('*', (req, res, next) => {
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    status: 'fail',
+    message: err.message
+  });
+});
+
 module.exports = app;
